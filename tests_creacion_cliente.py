ec570d38c9f0f8102e54bdddb738c782b64eb12c
Soysakiaytuno <leonardhopst@gmail.com>
2026-05-22 23:56:02 -0400

    test de creacion cliente happy path

---
+++ tests_creacion_cliente.py
@@ -0,0 +1,15 @@
+from unittest.mock import patch, MagicMock
+from modelo import BaseDeDatos
+
+@patch('modelo.create_client')
+def test_hu01_registrar_cliente_happy_path(mock_create_client):
+    mock_supabase = MagicMock()
+    mock_create_client.return_value = mock_supabase
+    mock_supabase.table().select().eq().execute().data = []
+
+    db = BaseDeDatos()
+    exito, mensaje = db.registrar_cliente("12345678", "Juan Perez")
+
+    assert exito is True
+    assert mensaje == "Cliente registrado exitosamente en la nube."
+    mock_supabase.table().insert.assert_called_once_with({"dni": "12345678", "nombre": "Juan Perez"})
